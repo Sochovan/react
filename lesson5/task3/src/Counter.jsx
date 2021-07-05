086b46d572c12a6b8a238f53c6957679d12541f9
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-05 19:50:31 +0300

    test1

---
+++ lesson5/task3/src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  render() {
+    return ()
+  }
+}
+
+export default Counter;

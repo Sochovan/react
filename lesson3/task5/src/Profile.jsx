f1d2a445519903d760c5b1a12c27518afd5cb929
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-05 14:08:53 +0300

    test3

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = (props) => {
   const age = moment(props.userData.birthDate).format("DD MMM YY");
   return (
     <div className="profile">
-      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName} `}</div>
+      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
       <div className="profile__birth">{`Was born ${age} in ${props.userData.birthPlace}`}</div>
     </div>
   );

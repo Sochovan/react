adb4c0eaafbfa5b2b2dd767388bfeb8757eada53
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-05 13:31:42 +0300

    test2

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 const Greeting = (props) => {
+  const age = Math.floor(
+    (new Date() - props.birthDate) / 1000 / 60 / 60 / 24 / 365
+  );
   console.log(props);
-  const birthDate = new Date(1984, 2, 18);
-  console.log(birthDate);
-  const yearsOld = (new Date() - birthDate) / 1000;
-
   return (
-    <div className="greeting">{`My name is ${props.firstName} ${
-      props.lastName
-    }. I'm ${Math.floor(yearsOld / 3600 / 24 / 365)} years old`}</div>
+    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}</div>
   );
 };
 
-// Greeting("John Doe");
 export default Greeting;
+
+// Greeting("John Doe");

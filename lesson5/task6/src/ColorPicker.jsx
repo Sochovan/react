7dd1d745181f55fcc0ef10807ba4437bb55ecf35
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-06 10:24:17 +0300

    test2

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -5,33 +5,38 @@ const AQUA = "Aqua";
 const BISQUE = "Bisque";
 
 class ColorPicker extends React.Component {
-  onChangeTextColor = (name) => {
-    const divElem = document.querySelector(".picker__title");
-    divElem.textContent = name;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      divTextElem: "",
+    };
+  }
+  onChangeTextColor = (colorName) => {
+    this.setState({
+      divTextElem: colorName,
+    });
   };
-  onClean = () => {
-    const divElem = document.querySelector(".picker__title");
-    divElem.textContent = "";
-  };
+
   render() {
     return (
       <div>
-        <div className="picker__title">Red</div>
+        <div className="picker__title">{this.state.divTextElem}</div>
         <div>
           <button
             className="picker__button picker__button_coral"
             onMouseOver={() => this.onChangeTextColor(CORAL)}
-            onMouseOut={() => this.onClean()}
+            onMouseOut={() => this.onChangeTextColor()}
           ></button>
           <button
             className="picker__button picker__button_aqua"
             onMouseOver={() => this.onChangeTextColor(AQUA)}
-            onMouseOut={() => this.onClean()}
+            onMouseOut={() => this.onChangeTextColor()}
           ></button>
           <button
             className="picker__button picker__button_bisque"
             onMouseOver={() => this.onChangeTextColor(BISQUE)}
-            onMouseOut={() => this.onClean()}
+            onMouseOut={() => this.onChangeTextColor()}
           ></button>
         </div>
       </div>
@@ -40,3 +45,40 @@ class ColorPicker extends React.Component {
 }
 
 export default ColorPicker;
+
+// class ColorPicker extends React.Component {
+//   onChangeTextColor = (name) => {
+//     const divElem = document.querySelector(".picker__title");
+//     divElem.textContent = name;
+//   };
+//   onClean = () => {
+//     const divElem = document.querySelector(".picker__title");
+//     divElem.textContent = "";
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <div className="picker__title">Red</div>
+//         <div>
+//           <button
+//             className="picker__button picker__button_coral"
+//             onMouseOver={() => this.onChangeTextColor(CORAL)}
+//             onMouseOut={() => this.onClean()}
+//           ></button>
+//           <button
+//             className="picker__button picker__button_aqua"
+//             onMouseOver={() => this.onChangeTextColor(AQUA)}
+//             onMouseOut={() => this.onClean()}
+//           ></button>
+//           <button
+//             className="picker__button picker__button_bisque"
+//             onMouseOver={() => this.onChangeTextColor(BISQUE)}
+//             onMouseOut={() => this.onClean()}
+//           ></button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ColorPicker;

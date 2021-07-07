642edbcb4050b30ddbdf039018f5df04cb7bca7b
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-07 14:45:12 +0300

    test1

---
+++ lesson7/task3/src/TransactionsList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Transaction from "./Transaction.jsx";
+
+
+const TransactionsList = ({ transactions }) => {
+  console.log(transactions);
+  return (
+    <ul className="transactions">
+      {transactions.map((transaction) => (
+        <Transaction key={transaction.id} {...transaction} />
+      ))}
+    </ul>
+  );
+};
+
+
+export default TransactionsList;

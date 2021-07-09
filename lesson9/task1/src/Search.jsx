0cc5e89bcd216469c19fc186b056ed461937ac0c
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-09 18:33:26 +0300

    test1

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = {
+    value: "",
+  };
+
+  handleChange = (event) => {
+    console.log();
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  search = (event) => {
+    event.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+    console.log(this.state.value);
+  };
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          className="search__input"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button className="search__button">Search</button>
+      </form>
+    );
+  }
+}
+
+export default Search;

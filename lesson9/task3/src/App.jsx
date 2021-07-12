f6ceb75a72764fb8649922eac43103e5b5d82181
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-12 13:08:52 +0300

    test1

---
+++ lesson9/task3/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import UserForm from "./UserForm.jsx";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  createUser = (user) => {
+    console.log(user);
+  };
+
+  render() {
+    return <UserForm onSubmit={this.createUser} />;
+  }
+}
+
+export default App;
---
+++ lesson9/task4/src/UserForm.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class UserForm extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+
+    this.state = {
+     
+    };
+  }
+
+  
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default UserForm;

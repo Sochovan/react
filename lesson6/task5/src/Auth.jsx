e318d65254fc5c99fe887812207b7bf8cfbd70be
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-13 12:29:09 +0300

    test8

--- lesson6/task5/src/Auth.jsx
+++ lesson6/task5/src/Auth.jsx
@@ -1,47 +1,51 @@
 import React from "react";
-import Spinner from "./Spinner.jsx";
 import Login from "./Login.jsx";
 import Logout from "./Logout.jsx";
+import Spinner from "./Spinner.jsx";
+
+// algo
+// 1. show login by default +++++
+// 2. show spinner for 2 sec after login click
+// 3. show logout
+// 4. show login after logout click
 
 class Auth extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoggedIn: true,
-      isSpinner: false,
-    };
-  }
-
-  handleLogin = () => {
-    this.setState({
-      isLoggedIn: true,
-      isSpinner: true,
-    });
-
-    setTimeout(() => {
-      this.setState({
-        isLoggedIn: false,
-        isSpinner: false,
-      });
-    }, 2000);
+  state = {
+    isLoggedIn: false,
+    isLogging: false,
   };
 
-  handleLogout = () => {
+  onLogin = () => {
     this.setState({
-      isLoggedIn: true,
-      isSpinner: false,
+      isLogging: true,
+    });
+    console.log("SET STATE CALLED!!!");
+    //input: func, ms
+    //output: undefined
+    setTimeout(() => {
+      this.setState({
+        isLoggedIn: true,
+        isLogging: false,
+      });
+    }, 2000);
+    console.log("LAST STEP");
+  };
+
+  onLogout = () => {
+    this.setState({
+      isLoggedIn: false,
     });
   };
 
   render() {
-    let button;
-    if (this.state.isLoggedIn) {
-      button = <Login onLogin={this.handleLogin} />;
-    } else {
-      button = <Logout onLogout={this.handleLogout} />;
+    const { isLogging, isLoggedIn } = this.state;
+    if (isLogging) {
+      return <Spinner size={50} />;
     }
-    return <div>{this.state.isSpinner ? <Spinner size={40} /> : button}</div>;
+    if (isLoggedIn) {
+      return <Logout onLogout={this.onLogout} />;
+    }
+    return <Login onLogin={this.onLogin} />;
   }
 }
 

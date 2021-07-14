9a7e5350c2409484b5812dd4347f0425f867be1e
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-14 10:27:19 +0300

    test6

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import ShoppingCart from "./ShoppingCart.jsx";
 import Profile from "./Profile.jsx";
 
-
 class Page extends Component {
   render() {
     return (
       <div className="page">
+        <h1 className="title">Hello, John Doe</h1>
         <main className="content">
           <ShoppingCart />
           <Profile />
--- lesson10/task2/src/UserForm.jsx
+++ lesson10/task2/src/UserForm.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 
 class UserForm extends React.Component {
+  state = {
+    userData: {
+      firstName: "Tom",
+      lastName: "Ford",
+    },
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      userData: {
+        ...this.state.userData,
+        [name]: value,
+      },
+    });
+  };
   render() {
     return (
       <form className="user-form">
@@ -8,13 +24,15 @@ class UserForm extends React.Component {
           type="text"
           name="firstName"
           className="user-form__input"
-          value="John"
+          value={this.state.userData.firstName}
+          onChange={this.handleChange}
         />
         <input
           type="text"
           name="lastName"
           className="user-form__input"
-          value="Doe"
+          value={this.state.userData.lastName}
+          onChange={this.handleChange}
         />
       </form>
     );

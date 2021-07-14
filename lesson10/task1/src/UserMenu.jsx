3be4706f04e1d5ebb042170339485ba3feb413fe
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-14 09:27:57 +0300

    test1

---
+++ lesson10/task1/src/UserMenu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UserMenu = ({ userData }) => {
+  if (!userData) {
+    return null;
+  }
+  const { name, avatar_url } = userData;
+  return (
+    <div className="menu">
+      <img src={avatar_url} alt="User avatar" className="menu__avatar" />
+      <span className="menu__name">{name}</span>
+    </div>
+  );
+};
+
+export default UserMenu;
